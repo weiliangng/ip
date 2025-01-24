49330610b717076516f14a7622cc6d2b81715b65
weiliangng <46848740+weiliangng@users.noreply.github.com>
2025-01-24 15:51:05 +0800

    Update and rename Duke.java to Blarney.java

---
+++ src/main/java/Blarney.java
@@ -0,0 +1,11 @@
+public class Blarney {
+    public static void main(String[] args) {
+        String name = "Blarney";
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm " + name + "!");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+}
