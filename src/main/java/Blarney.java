d5af3a34cbf342e113c181762b64d9377897e43e
weiliangng <46848740+weiliangng@users.noreply.github.com>
2025-01-30 22:54:56 +0800

    Level-2

--- src/main/java/Blarney.java
+++ src/main/java/Blarney.java
@@ -4,6 +4,8 @@ public class Blarney {
     public static void main(String[] args) {
         String name = "Blarney";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("________________________________");
         System.out.println("Hello! I'm " + name + "!");
@@ -18,11 +20,20 @@ public class Blarney {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("________________________________");
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount++] = input;
+                    System.out.println("________________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("________________________________");
+                }
             }
-
-            System.out.println("________________________________");
-            System.out.println(" " + input);
-            System.out.println("________________________________");
         }
 
         scanner.close();
