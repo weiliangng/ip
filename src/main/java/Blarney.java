259c9e0087d59f53807604f3a478e47de2e1f078
weiliangng <46848740+weiliangng@users.noreply.github.com>
2025-01-30 22:43:20 +0800

    Level-1

--- src/main/java/Blarney.java
+++ src/main/java/Blarney.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Blarney {
     public static void main(String[] args) {
         String name = "Blarney";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("________________________________");
         System.out.println("Hello! I'm " + name + "!");
         System.out.println("What can I do for you?");
         System.out.println("________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("________________________________");
+                break;
+            }
+
+            System.out.println("________________________________");
+            System.out.println(" " + input);
+            System.out.println("________________________________");
+        }
+
+        scanner.close();
     }
 }
